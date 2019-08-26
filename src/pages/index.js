2f68be397a149d7ecf3fec0e24537b437ec5cc75
Carson <dragonchen66@gmail.com>
2019-08-26 17:13:04 -0400

    Updated typography. Updated images on the index page hero area.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,60 @@
 import React from "react"
-// import { graphql, Link } from "gatsby"
-// import Img from "gatsby-image"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
 
-import iOSDownload from "../images/button/download-ios.png"
-import androidDownload from "../images/button/download-android.png"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home"/>
-    <div>INTRODUCING</div>
-    <h1>Color Collect</h1>
-    <p>A color app for everyone.</p>
-    <img src={iOSDownload} alt="Download Color Collect for iOS"/>
-    <img src={androidDownload} alt="Download Color Collect for Android"/>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      ios: file(relativePath: { eq: "button/download-ios.png" }) {
+        childImageSharp {
+          fixed(width: 48, height: 48) {
+            base64
+            width
+            height
+            src
+            srcSet
+          }
+        }
+      }
+      
+      android: file(relativePath: { eq: "button/download-android.png" }) {
+        childImageSharp {
+          fixed(width: 48, height: 48) {
+            base64
+            width
+            height
+            src
+            srcSet
+          }
+        }
+      }
+    }
+  `,
+  )
+  return (
+    <Layout>
+      <SEO title="Home"/>
+      <div className="type--note">INTRODUCING</div>
+      <h1 className="type--h2" style={{ margin: 0 }}>Color Collect</h1>
+      <p>A color app for everyone.</p>
+      <Link to={`https://apps.apple.com/cn/app/id1439521846`}>
+        <Img fixed={data.ios.childImageSharp.fixed}
+             title={`Download Color Collect for iOS`}
+             alt={`Download Color Collect for iOS`}
+        />
+      </Link>
+      <Link to={`https://play.google.com/store/apps/details?id=com.wizeyes.colorcapture`}>
+        <Img fixed={data.android.childImageSharp.fixed}
+             title={`Download Color Collect for Android`}
+             alt={`Download Color Collect for Android`}
+        />
+      </Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
