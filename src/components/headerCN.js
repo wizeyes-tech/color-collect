4c9dd3a2f9eef22e95974f78e7b1168ba2ebba07
Carson <dragonchen66@gmail.com>
2019-08-29 11:20:08 -0400

    Added tablet view of header.

--- src/components/headerCN.js
+++ src/components/headerCN.js
@@ -2,20 +2,28 @@ import { Link } from "gatsby"
 import Img from "gatsby-image"
 import PropTypes from "prop-types"
 import React from "react"
-import headerStyles from "./header.module.css"
+import styles from "./header.module.css"
 
 
 const HeaderCN = ({ siteTitle, logo, iconMenu, iconClose, iconLan, lanTitle }) => {
   return (
-    <header className={headerStyles.header}>
-      <Link to={`/`}>
+    <header className={styles.header}>
+      <Link to={`/`} className={styles.language}>
         <Img fixed={iconLan} title={lanTitle} alt={lanTitle}/>
       </Link>
-      <Link to="/" className={headerStyles.logo}>
+      <Link to="/" className={styles.logo}>
         <Img fixed={logo} title={siteTitle} alt={siteTitle + ` Logo`}/>
       </Link>
-      <Img fixed={iconMenu} title="Open Menu" alt="Open Menu"/>
-      {/*<Img fixed={iconClose} title="Close Menu" alt="Close Menu" />*/}
+      <div className={styles.menu}>
+        <div className={styles.menuTrigger}>
+          <Img fixed={iconMenu} title="Open Menu" alt="Open Menu"/>
+          {/*<Img fixed={iconClose} title="Close Menu" alt="Close Menu" />*/}
+        </div>
+        <div className={styles.menuList}>
+          <Link to={`/`}>探索色卡</Link>
+          <Link to={`/`}>合作应用</Link>
+        </div>
+      </div>
     </header>
   )
 }
--- src/components/headerEN.js
+++ src/components/headerEN.js
@@ -2,20 +2,27 @@ import { Link } from "gatsby"
 import Img from "gatsby-image"
 import PropTypes from "prop-types"
 import React from "react"
-import headerStyles from "./header.module.css"
+import styles from "./header.module.css"
 
 
 const HeaderEN = ({ siteTitle, logo, iconMenu, iconClose, iconLan, lanTitle }) => {
   return (
-    <header className={headerStyles.header}>
-      <Link to={`/cn/`}>
+    <header className={styles.header}>
+      <Link to={`/cn/`} className={styles.language}>
         <Img fixed={iconLan} title={lanTitle} alt={lanTitle} />
       </Link>
-      <Link to="/cn/" className={headerStyles.logo}>
+      <Link to="/cn/" className={styles.logo}>
         <Img fixed={logo} title={siteTitle} alt={siteTitle + ` Logo`} />
       </Link>
-      <Img fixed={iconMenu} title="Open Menu" alt="Open Menu" />
-      {/*<Img fixed={iconClose} title="Close Menu" alt="Close Menu" />*/}
+      <div className={styles.menu}>
+        <div className={styles.menuTrigger}>
+          <Img fixed={iconMenu} title="Open Menu" alt="Open Menu" />
+          {/*<Img fixed={iconClose} title="Close Menu" alt="Close Menu" />*/}
+        </div>
+        <div className={styles.menuList}>
+          <Link to={`/`}>Palettes</Link>
+        </div>
+      </div>
     </header>
   )
 }
