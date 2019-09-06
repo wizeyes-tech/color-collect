dcbd9852d4a0ae16a3942dc9471d531475c02bdb
Carson <dragonchen66@gmail.com>
2019-09-06 17:29:14 -0400

    Added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Color Collect Site`,
+        short_name: `Color Collect`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#F8FAFC`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     {
